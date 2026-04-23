eb85452c9794e872664b02fae6b4f85fb0a13da1
ANGMAR <wngarinech@gmail.com>
2026-04-23 23:08:30 +0300

    Устранён циклический импорт: удалён импорт bot из handlers/start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,6 @@
 from aiogram import types, Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from bot import bot
 
 router = Router()
 
@@ -15,5 +14,5 @@ async def start_command(message: Message):
 async def on_user_chat(message: types.Message):
     if message.new_chat_members:
         for new_member in message.new_chat_members:
-            if new_member.id != (await bot.get_me()).id:
+            if new_member.id != (await message.get_me()).id:
                 await message.answer(f"Привет, {new_member.full_name}! Добро пожаловать в группу!")
